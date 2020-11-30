9d8ed0990015993d7902573220757ab0eaf6087f
илья <vlasov.ilia-93@mail.ru>
2020-11-30 17:48:13 +0700

    resave

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,21 @@
 "use strict";
 
-let a = "Hello";
+const numberOfFilms = prompt("Сколько фильмов вы посмотрели за свою жизнь?", "");
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt("Один из просмотреннх фильмов?", ""),
+   b = prompt("На скольок вы его оцените?", ""),
+   c = prompt("Один из просмотреннх фильмов?", ""),
+   d = prompt("На скольок вы его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
